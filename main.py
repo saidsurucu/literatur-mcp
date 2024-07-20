e9d8411b4247cf9d466546340a4668af1ac9a51d
saidsurucu <saidsrc@gmail.com>
2024-07-20 20:29:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ async def get_article_details(article_url: str) -> dict:
         'citation_language', 'DC.Type', 'DC.Identifier.pageNumber', 'DC.Identifier.URI',
         'citation_reference', 'citation_journal_abbrev', 'citation_abstract_html_url',
         'DC.Type.articleType', 'DC.Identifier', 'citation_funding_source',
-        'stats_trdizin_citation_updated_at', 'DC.Identifier.DOI'
+        'stats_trdizin_citation_updated_at', 'DC.Identifier.DOI', 'DC.Title'
     ]:
         details.pop(key, None)
 
